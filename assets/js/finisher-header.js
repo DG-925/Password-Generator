cd6c6e0d91c1535b57f37d8bdddd991301d7e94f
DG <adham.kassem925@gmail.com>
2025-02-07 05:11:32 +0200

    Downloaded tailwind

---
+++ assets/js/finisher-header.js
@@ -0,0 +1,37 @@
+new FinisherHeader({
+  "count": 10,
+  "size": {
+    "min": 300,
+    "max": 1500,
+    "pulse": 0
+  },
+  "speed": {
+    "x": {
+      "min": 0.1,
+      "max": 0.6
+    },
+    "y": {
+      "min": 0.1,
+      "max": 0.6
+    }
+  },
+  "colors": {
+    "background": "#131313",
+    "particles": [
+      "#cccccc",
+      "#aaaaaa",
+      "#888888",
+      "#666666",
+      "#444444"
+    ]
+  },
+  "blending": "overlay",
+  "opacity": {
+    "center": 0.5,
+    "edge": 0.05
+  },
+  "skew": 0,
+  "shapes": [
+    "c"
+  ]
+});
